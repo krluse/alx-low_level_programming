4b8db99e5ad3429de96a6e8416b20ea317090208
krluse <77019423+krluse@users.noreply.github.com>
2023-05-18 23:37:49 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
 int is_digit(char *s)		
 {
 	int i = 0;
-	while (s[i])	
+	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
@@ -28,7 +28,7 @@ int is_digit(char *s)
 int _strlen(char *s)		
 {
 	int i = 0;
-	while (s[i] != '\0')	
+	while (s[i] != '\0')
 	{
 		i++;	
 	}
